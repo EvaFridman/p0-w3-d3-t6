e4fc6ed3be10068188b0b96c44b9aa6bb8dea40d
Eva Fridman <fridman.1999@mail.ru>
2026-07-01 21:06:58 +0300

    release

---
+++ phones.js
@@ -0,0 +1,24 @@
+const phones = [
+  "8(999)123-45-67",
+  "+7 999 1234567",
+  "89991234567",
+  "7 (999) 123 45 67",
+  "12345",
+];
+
+function formatter(phone) {
+    for (let i = 0; i < phone.length; i++) {
+    
+        const result = phone[i].replace(/\D/g, "").replace(/^.(\d{3})(\d{3})(\d{2})(\d{2})$/, (match, g1, g2, g3, g4) => {
+          return `+7 ${g1} ${g2}-${g3}-${g4}`;
+        });
+    
+        if (result.includes(" ")) {
+          console.log(`${phone[i]} → ${result}`);
+        } else {
+          console.log(`${phone[i]} -> не похоже на телефон`);
+        }
+      }
+}
+
+formatter(phones);
